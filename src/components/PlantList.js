46e239d025061c3e402d57096f5b1b975eb161c3
jubalthang <jubalthang@gamil.com>
2021-11-15 09:59:20 -0600

    assignment complete

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+  // const newPlantlists = plants.map(plant => Object.assign({...plant}, {isSold : false}))
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+   
+    <ul className="cards">{/* render PlantCards components in here */}
+    {/* {newPlantlists.map(plant => <PlantCard key={plant.id} plant={plant} onSold={handleSold}/>)} */}
+    {plants.map(plant => <PlantCard key={plant.id} plant={plant}/>)}
+    </ul>
+  
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlanet] = useState([])
+  const [searchText, setSearchText] = useState('')
+  const [isShowSold, setIsshowSold] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(data => {
+      const newPlantlist = data.map(d => Object.assign({...d}, {isSold:false}))
+      setPlanet(newPlantlist)
+    })
+    .catch(error => console.error('There is an error fetching plants', error))
+  },[])
+
+  // function handleSold(plant) {
+  //   const newPlent = {...plant, isSold: true}
+  //   // console.log(newPlent)
+  //   setSoldlist([...soldlist, plant ])
+  // }
+
+  const filtered = (
+    isShowSold ? plants.filter(plant => plant.isSold) :
+    plants.filter((plant) => plant.name.toLowerCase().includes(searchText.toLowerCase()))
+  )
+
+  function handleAddNewPlant(plant) {
+
+    setPlanet([...plants], plant)
+
+    console.log(plants)
+  }
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAdd={handleAddNewPlant}/>
+      <Search searchText={searchText} onSearch={setSearchText}/>
+      <button onClick={() => setIsshowSold(!isShowSold)}>Show Sold plants</button>
+      <PlantList plants={filtered}/>
     </main>
   );
 }
